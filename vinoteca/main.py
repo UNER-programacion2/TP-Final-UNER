ae922b4947a8946bd65481087167befe86beccd3
Santiago Bertorello <santinbertorello@gmail.com>
2024-11-15 00:29:41 -0300

    templates html y static css Co-authored-by: Gabr!ela. <gabrielasan07@gmail.com> Co-authored-by: Victoria Inés Avalos <vickkyavalos@users.noreply.github.com>

--- vinoteca/main.py
+++ vinoteca/main.py
@@ -1,5 +1,5 @@
 # Flask
-from flask import Flask
+from flask import Flask,jsonify, render_template
 from flask_restful import Api
 
 from vinoteca import Vinoteca
@@ -20,4 +20,15 @@ if __name__ == "__main__":
     api.add_resource(RecursoVino, '/api/vinos/<id>')
     api.add_resource(RecursoVinos, '/api/vinos')
 
+    
+
+    
+    
+
+    
+    @app.route('/api/vinos')
+    def get_vinos():
+        return jsonify(vinos)
+
     app.run(debug=True)
+
