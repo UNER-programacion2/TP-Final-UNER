d4c51b61e6af77eabcd4b0f0b5465fa225cc8007
Victoria Ines <inessavalos14@gmail.com>
2024-11-16 01:15:36 -0300

    STYLE: nueva interfaz
    Co-authored-by: Gabr!ela. <gabrielasan07@gmail.com>
    Co-authored-by: SantiagoBertorello <SantiagoBertorello@users.noreply.github.com>

--- vinoteca/main.py
+++ vinoteca/main.py
@@ -1,5 +1,5 @@
 # Flask
-from flask import Flask,jsonify, render_template
+from flask import Flask, send_from_directory
 from flask_restful import Api
 
 from vinoteca import Vinoteca
@@ -12,6 +12,11 @@ if __name__ == "__main__":
 
     app = Flask(__name__)
 
+#redirigir a la página principal index
+    @app.route('/')
+    def index():
+        return send_from_directory('.', 'index.html')
+
     api = Api(app)
     api.add_resource(RecursoBodega, '/api/bodegas/<id>')
     api.add_resource(RecursoBodegas, '/api/bodegas')
@@ -20,15 +25,6 @@ if __name__ == "__main__":
     api.add_resource(RecursoVino, '/api/vinos/<id>')
     api.add_resource(RecursoVinos, '/api/vinos')
 
-    
-
-    
-    
-
-    
-    #@app.route('/api/vinos')
-    #def get_vinos():
-    #   return jsonify(vinos)
 
     app.run(debug=True)
 
